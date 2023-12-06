56d65a79321dffdc843ce6bbb06435d0f9f71782
Pawel Laba <labapawel@gmail.com>
2023-12-06 08:34:23 +0100

    zmiany

--- public/script.js
+++ public/script.js
@@ -4,6 +4,14 @@ let alarm = $('.alarm');
 let servertime = $(".czasserwera");
 let przerwy = $(".przerwy");
 let edycja = false;
+let dodajdzwonek = $(".dodajdzwonek");
+
+dodajdzwonek.addEventListener('click', e=>{
+    
+    let i = przerwy.querySelectorAll('.input-group').length;
+    edycja=true;
+    dodajelem(i+1, 0);
+})
 
 alarm.addEventListener('click', e=>{
     fetch('/setalarm?alarm=true')
@@ -13,23 +21,26 @@ alarm.addEventListener('click', e=>{
         })
 })
 
+function dodajelem(num, wart){
+    let inp = `<div class="input-group flex-nowrap">
+    <span class="input-group-text" id="addon-wrapping">${num}</span>
+    <input type="numeric" value="${wart}" class="form-control" placeholder="Długość przerwy" 
+        aria-label="Długość przerwy"
+         aria-describedby="addon-wrapping">
+         <span class="btn btn-outline-secondary btn-danger" id="basic-addon2">
+          <span class="icon icon-trash danger"></span>
+         </span>
+        
+  </div>`;
+
+  
+    przerwy.innerHTML += inp;
+}
+
 function budujEdycjeDzwonkow(dzwonki){
     przerwy.innerHTML = '';
     dzwonki.forEach((e,i)=>{
-        let inp = `<div class="input-group flex-nowrap">
-        <span class="input-group-text" id="addon-wrapping">${i+1}</span>
-        <input type="numeric" value="${e}" class="form-control" placeholder="Długość przerwy" 
-            aria-label="Długość przerwy"
-             aria-describedby="addon-wrapping">
-             <span class="btn btn-outline-secondary btn-danger" id="basic-addon2">
-              <span class="icon icon-trash danger"></span>
-             </span>
-            
-      </div>`;
-
-      
-        przerwy.innerHTML += inp;
-
+        dodajelem(i+1, e);
     })
 }
 
